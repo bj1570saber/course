994e80671518227babeb742a60df660e7e77cce7
bj1570saber <bj1570saber@gmail.com>
2020-10-12 23:02:04 -0700

    1. Add dto layer, for controller and service layer; 2. add chapterDto; 3. modify ChapterService, return value change to 'ChapterDto'

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,6 +1,7 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
 import com.course.server.domain.Chapter;
+import com.course.server.dto.ChapterDto;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-//@RequestMapping("/system")
+
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
     @Resource
     private ChapterService chapterService;
 
     @RequestMapping("/chapter")
-    public List<Chapter> chapter(){
+    public List<ChapterDto> chapter(){
         return chapterService.list();
     }
 }
